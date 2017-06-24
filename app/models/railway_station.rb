a5465eb468f682c7d519c17bd19627783069fc00
Alexandr <microposts@gmail.com>
2017-06-24 11:56:08 +0300

    added update_position to RailwayStation model

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,4 +2,9 @@ class RailwayStation < ApplicationRecord
   validates :title, presence: true
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
+
+  def update_position(route, position)
+    station_route = railway_stations_routes.where(route: route).first
+    station_route.update(position: position) if station_route
+  end
 end
