2a882805a44b4fbaaafcf21e6d7f17b48f6a8b89
Alexandr <microposts@gmail.com>
2017-06-26 12:27:34 +0300

    added validates number uniq for wagon

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,4 +1,5 @@
 class Wagon < ApplicationRecord
+  validates :number, uniqueness: true
   belongs_to :train
   belongs_to :first_station, class_name: "RailwayStation", foreign_key: :first_station_id
   belongs_to :last_station, class_name: "RailwayStation", foreign_key: :last_station_id
