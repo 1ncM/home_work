0311a2cbb40eef84b1f98d45e4800b8e7e0821a9
Alexandr <microposts@gmail.com>
2017-06-27 12:31:49 +0300

    added sorting_wagon feature

--- app/models/train.rb
+++ app/models/train.rb
@@ -3,4 +3,12 @@ class Train < ApplicationRecord
   belongs_to :route
   has_many :tickets
   has_many :wagons
+
+  def sorted_wagons
+    if self.sorting_wagons
+      self.wagons.asc
+    else
+      self.wagons.desc
+    end
+  end
 end
